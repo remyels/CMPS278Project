668154206e3332cbbdb35aed6ff76c3c573f2f33
Remy <rae66@mail.aub.edu>
2019-04-21 20:18:00 +0300

    tala meze3jeh

--- CloudFund/connect/connectPDO.php
+++ CloudFund/connect/connectPDO.php
@@ -1,6 +1,6 @@
 <?php
 // create PDO object that connects to my database
-$db = new PDO("mysql:host=localhost;dbname=cloudfund", "root", "root123");
+$db = new PDO("mysql:host=localhost;dbname=cloudfund", "root", "");
 // set exception and error mode
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
